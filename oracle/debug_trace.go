bcb8510dcc6b0e4668c3a6372c91ae2baea077a0
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2013-05-30 11:49:00 +0200

    fix build: liccor inserted the copyright before the build tag, thus invalidating it...

--- oracle/debug_trace.go
+++ oracle/debug_trace.go
@@ -1,3 +1,5 @@
+// +build trace
+
 /*
    Copyright 2013 Tamás Gulácsi
 
@@ -13,7 +15,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-// +build trace
 
 package oracle
 
